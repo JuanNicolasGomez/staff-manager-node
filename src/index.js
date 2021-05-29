2f1548cca1aae45f1ec24b3672c39f9048ca4524
Nicolás Gómez <juan.gomez-more@mail.escuelaing.edu.co>
2021-05-29 13:07:26 -0500

    refactor user authentication

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const morgan = require('morgan');
+const errorHandler = require('./helpers/errorHandler');
 
 app.set('port', process.env.PORT || 3000);
 
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use(cors());
 
 app.use('/api', require('./router'));
 
-app.listen(app.get('port'),() => {
-    console.log(`Server on port ${3000}`)
+app.use(errorHandler);
+
+const port = process.env.NODE_ENV === 'production' ? 80 : 3000;
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${port}`)
 })
--- src/router.js
+++ src/router.js
@@ -1,55 +1,24 @@
 const Router = require('express');
-const jwt = require("jsonwebtoken");
 const staffController = require('./controllers/staffController');
 const technologiesController = require('./controllers/technologiesController');
-
+const usersController = require('./controllers/usersController');
+const authorize = require('./helpers/authorize');
+const Role = require('./helpers/role');
 
 const router = new Router();
 
-router.use(setUser);
+router.post('/users/authenticate', usersController.authenticate);
+router.get('/users/', authorize(Role.Admin), usersController.getAll);
+router.get('/users/:id', authorize(), usersController.getById);
 
-router.post('/login', (req , res) => {
-    const user = req.body.user;
-    jwt.sign({user}, 'secretkey', {expiresIn: '1h'}, (err, token) => {
-        res.json({
-            token
-        });
-    });
-});
+router.get('/staff', staffController.getStaff);
+router.post('/staff', staffController.createStaff);
+router.put('/staff/:id', staffController.updateStaff);
+router.delete('/staff/:id', staffController.deleteStaff);
 
-router.get('/staff', verifyToken, staffController.getStaff);
-router.post('/staff', verifyToken, staffController.createStaff);
-router.put('/staff/:id', verifyToken, staffController.updateStaff);
-router.delete('/staff/:id', verifyToken, staffController.deleteStaff);
-
-router.get('/technologies', verifyToken, technologiesController.getTechnologies);
-router.post('/technologies', verifyToken, technologiesController.createTechnologies);
-router.put('/technologies/:id', verifyToken, technologiesController.updateTechnologies);
-router.delete('/technologies/:id', verifyToken, technologiesController.deleteTechnologies);
-
-function verifyToken(req, res, next){
-    const bearerHeader =  req.headers['authorization'];
-
-    if(typeof bearerHeader !== 'undefined'){
-        const bearerToken = bearerHeader.split(" ")[1];
-        req.token  = bearerToken;
-        jwt.verify(req.token, 'secretkey', (error, authData) => {
-            if(error){
-                res.sendStatus(403);
-            }
-        });
-        next();
-    }else{
-        res.sendStatus(403);
-    }
-}
-
-function setUser(req, res, next) {
-    const userId = req.body.userId
-    if (userId) {
-      req.user = users.find(user => user.id === userId)
-    }
-    next()
-  }
+router.get('/technologies', technologiesController.getTechnologies);
+router.post('/technologies', technologiesController.createTechnologies);
+router.put('/technologies/:id', technologiesController.updateTechnologies);
+router.delete('/technologies/:id', technologiesController.deleteTechnologies);
 
 module.exports = router;
