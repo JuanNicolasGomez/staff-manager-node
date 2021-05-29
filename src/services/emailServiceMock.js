8d7e189b4778ffebd72d1ea3d99d6891f0491405
Nicolás Gómez <juan.gomez-more@mail.escuelaing.edu.co>
2021-05-29 16:17:55 -0500

    add email error log

--- src/services/emailServiceMock.js
+++ src/services/emailServiceMock.js
@@ -3,5 +3,9 @@ module.exports = {
 };
 
 async function sendEmail({title, to, content}) {
+    if (!title || !to || !content) {
+        console.log(`Bad params for email`);
+        return;
+    }
     console.log(`Mock email sent Succesfully: ${title, to, content}`);
 }
